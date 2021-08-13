6bf60e85e6b6de33dd4d93541fdebef3c5f83869
Rob Trehy <robert.trehy@outlook.com>
2021-08-13 09:00:07 +0100

    Version 1.0.0

--- include/_html.php
+++ include/_html.php
@@ -3,11 +3,11 @@
     foreach ($this->settings['items'] as $item) : ?>
         <?php $url = (strpos(strtolower($item['linkURL']), 'fireflycloud.net/resource.aspx?id=')) ? $item['linkURL'].'&officient=on' : $item['linkURL']; ?>
         <a id="item<?php echo $i; ?>" href="<?php echo $url; ?>" target="_blank"
-            class="relative block hover:bg-gray-50 dark:hover:bg-gray-700 <?php echo isset($item['linkIcon']) ? $item['linkIcon'] : ''; ?>">
+            class="relative block hover:bg-gray-50 dark:hover:bg-gray-700 dark:text-white <?php echo isset($item['linkIcon']) ? $item['linkIcon'] : ''; ?>">
             <h5 class="font-semibold text-lg"><?php echo $item['linkName']; ?></h5>
             <?php if ($item['linkDesc'] <> '') : ?>
                 <p class="mb-1"><?php echo $item['linkDesc']; ?></p>
             <?php endif; ?>
         </a>
-    <?php endforeach; ?>
+    <?php $i++; endforeach; ?>
 </div>
